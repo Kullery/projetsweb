4fece993b95665e1ab4e74bd93d963129cf803fb
Louis Duhesme <duhesme.louis@gmail.com>
2024-11-14 16:08:06 +0100

    v2.2

--- Projetvf/mission 6/java.js
+++ Projetvf/mission 6/java.js
@@ -1,34 +1,5 @@
-function total() {
-    let t1 = document.getElementById('p1').value * document.getElementById('q1').value;
-    let t2 = document.getElementById('p2').value * document.getElementById('q2').value;
-    let t3 = document.getElementById('p3').value * document.getElementById('q3').value;
-
-    document.getElementById('t1').value = t1.toFixed(2);
-    document.getElementById('t2').value = t2.toFixed(2);
-    document.getElementById('t3').value = t3.toFixed(2);
-
-    let grandTotal = t1 + t2 + t3;
-
-    document.getElementById('totalAmount').innerText = grandTotal.toFixed(2);
-}
-
-function reset() {
-    document.getElementById('a1').value = '';
-    document.getElementById('p1').value = '';
-    document.getElementById('q1').value = '';
-    document.getElementById('t1').value = '';
-
-    document.getElementById('a2').value = '';
-    document.getElementById('p2').value = '';
-    document.getElementById('q2').value = '';
-    document.getElementById('t2').value = '';
-
-    document.getElementById('a3').value = '';
-    document.getElementById('p3').value = '';
-    document.getElementById('q3').value = '';
-    document.getElementById('t3').value = '';
-
-    document.getElementById('totalAmount').innerText = '0.00';
+function certifs(){
+    window.open('certifs.pdf', '_blank');
 }
 
 function back1(){
